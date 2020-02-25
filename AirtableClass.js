434e27bca440ad0d473f7eab642e9343fcb288c5
mrmotes <44508091+mrmotes@users.noreply.github.com>
2020-02-25 15:28:42 -0800

    Create AirtableClass.js
    The beginning of an Airtable class which will retrieve Airtable records for use in Google Apps Script.

---
+++ AirtableClass.js
@@ -0,0 +1,27 @@
+const apiKey = "keysXXXXXXXXXXX";
+const baseId = "appXXXXXXXXXXXX";
+const tableName = "XXXXXXXXXXX";
+const viewId = "viwXXXXXXXXXXX";
+
+class AirtableBase {
+  constructor(apiKey, baseId, tableName, viewId) {
+    this._apiKey = apiKey;
+    this._baseId = baseId;
+    this._tableName = tableName;
+    this._viewId = viewId;
+  }
+
+  getAirtableDataObject() {
+    let offset = 0;
+    let records = [];
+    while (offset !== null) {
+      let url = `https://api.airtable.com/v0/${this._baseId}/${encodeURIComponent(this._tableName)}?api_key=${this._apiKey}&view=${this._viewId}&offset=${offset}`;
+      let options = { "method" : "GET" };
+      let response = JSON.parse(UrlFetchApp.fetch(url, options));
+      records.push.apply(records, response.records);
+      Utilities.sleep(200);
+      response.offset ? offset = response.offset : offset = null;
+    }
+    return records;
+  }
+}
